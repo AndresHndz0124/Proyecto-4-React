e5125e72afc4653aec431a61373274b829a6c0ab
AndresHndz0124 <anfhernandezve@unal.edu.co>
2023-02-22 20:19:32 -0500

    Update Header.js

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -6,16 +6,16 @@ function Header() {
         <header>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
                 <div className="container header">
-                    <a className="navbar-brand" href="#">
+                    <a className="navbar-brand" href="!#">
                         <img src="https://reverent-banach-cb9630.netlify.app/logo-ucamp-dark.png" alt="..." height="60"></img>
                     </a>
                     <div className="collapse navbar-collapse split" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Aplicación CRUD</a>
+                                <a className="nav-link active" aria-current="page" href="!#">Aplicación CRUD</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Proyecto No. 4: Restaurant App</a>
+                                <a className="nav-link" href="!#">Proyecto No. 4: Restaurant App</a>
                             </li>
                         </ul>
 
