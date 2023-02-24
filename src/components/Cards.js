6ccdbb38790227493b58eedb531dc38b5539804f
AndresHndz0124 <anfhernandezve@unal.edu.co>
2023-02-23 23:24:41 -0500

    Props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,25 @@
 import React from "react"
 import ListItem from "./List_label";
+import Mainprop from "./Main";
 
 export default function Cards(props) {
-    const { items,renderItem } = props;
+    const { items } = props;
+
     return (
-        <div className='card_separate'>
-            {items.map(item => <ListItem>{renderItem(item)} </ListItem>)}
-        </div>
+        items.map((item) => {
+            return (
+                <div className='card'>
+                    <div className='card_separate'>
+                        <h4>Nombre: {item.name}</h4>
+                        <p>Telefono: {item.Phone}</p>
+                        <p>Email: {item.email}</p>
+                        <p>Numero de personas: {item.Number_persons}</p>
+                        <p>Fecha: {item.Date}</p>
+                    </div>
+                    <button type="button" class="btn btn-danger" onClick={() => { Mainprop.deleteReserv(item.id) }}>Delete</button>
+
+                </div>
+            )
+        })
     )
 }
